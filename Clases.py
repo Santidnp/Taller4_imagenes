7c291e2fd57838c1811a2a7fd011af7d9a53c458
Santidnp <sngh966.sg@gmail.com>
2021-09-27 18:27:24 -0500

    Primer desarrollo de poligonos aleatorios con problemas

--- Clases.py
+++ Clases.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import random
 
 class Quadrilateral:
 
@@ -10,42 +11,53 @@ class Quadrilateral:
             raise ValueError('N solo puede número  par')
         #cyan = (255 255 0)
 
-        img = np.zeros((N,N,3))
-        b, g, r = cv2.split(img)
+        self.img = np.zeros((N,N,3))
+        b, g, r = cv2.split(self.img)
         b += 255
         g += 255
-        img = cv2.merge([b, g, r])
-        print(img.shape)
-        cv2.imshow("Fondo",img)
+        self.img = cv2.merge([b, g, r])
+        print(self.img.shape)
+        cv2.imshow("Fondo",self.img)
         cv2.waitKey(0)
 
 
     def generate(self):
-        Lados = int(np.random.uniform(3, 11, 1))
-        pass
+        #Magenta (255,0,255)
+        Lados = int(np.random.uniform(3, 8, 1))
+        Poligono = [[random.randrange(1,512, 1) for col in range(2)] for row in range(Lados)]
+
+        Poligono = np.array([Poligono],np.int32)
+
+        print(Lados)
+
+        img_mod =  cv2.polylines(self.img, [Poligono], True, (255,0,255), thickness=3)
+        cv2.imshow('Shapes', img_mod)
+        cv2.waitKey(0)
+        print(Poligono)
 
 
-print(int(np.random.uniform(3,11,1)))
+#print(int(np.random.uniform(3,11,1)))
 
 a = Quadrilateral(512)
-
-n = 100
-col1 = np.arange(3,3*n,3)
-col2 = np.arange(1,n)
-matrix = np.hstack((col1.reshape(n-1,1), col2.reshape(n-1,1)))
-
-z = np.array([[[40,160],[120,100],[200,160],[160,240],[80,240]]], np.int32)
-triangle = np.array([[[240, 130], [380, 230], [190, 280]]], np.int32)
-
-print(matrix.shape)
-
-print(z.shape)
-
-
-print(triangle.shape)
-a = [1,2]
-
-b = [[]]
-
-c = b.append(a)
-print(c)
+a.generate()
+#
+# n = 100
+# col1 = np.arange(3,3*n,3)
+# col2 = np.arange(1,n)
+# matrix = np.hstack((col1.reshape(n-1,1), col2.reshape(n-1,1)))
+#
+# z = np.array([[[40,160],[120,100],[200,160],[160,240],[80,240]]], np.int32)
+# triangle = np.array([[[240, 130], [380, 230], [190, 280]]], np.int32)
+#
+# print(matrix.shape)
+#
+# print(z.shape)
+#
+#
+# print(triangle.shape)
+# a = [1,2]
+#
+# b = [[]]
+#
+# c = b.append(a)
+# print(c)
